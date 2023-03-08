a289c27c6b8083d1b7175553e17a9d07d634bfb1
toxicbloud <fanta.r55000@gmail.com>
2023-03-08 22:03:41 +0100

    fix erreur lorsqu'on envoie des données non valide

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,6 +1,6 @@
 import hashlib
 from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Form
-from pydantic import BaseModel, parse_obj_as
+from pydantic import BaseModel, ValidationError, parse_obj_as
 from app.models.user import UserInFront, UserInFrontWithPhone, UserInToken
 from app.models.user import UserInRegister, User
 from tortoise.exceptions import IntegrityError
@@ -76,6 +76,8 @@ async def register(firstname: str = Form(..., description="Prénom de l'utilisat
     except IntegrityError as e:
         print(e)
         raise HTTPException(status_code=400, detail="Email already registered")
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=e.errors()[0]["msg"])
 
 
 @router.get("/")
