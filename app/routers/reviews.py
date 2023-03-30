11b3c3b9e01f7559ea5d5cd5ae43b43245a7a332
Julien Feraux <56791765+julienferaux@users.noreply.github.com>
2023-03-30 12:15:55 +0200

    Update reviews.py
    merge

--- app/routers/reviews.py
+++ app/routers/reviews.py
@@ -8,15 +8,7 @@ from app.utils.tokens import get_user_in_token
 
 
 router = APIRouter()
-@router.delete("/{review_id}")
-async def delete_review(review_id: int, user: UserInToken = Depends(get_user_in_token)):
-    review = await Review.get(id=review_id)
-    if review is None :
-        raise HTTPException(status_code=404, detail="Review does not exist")
-    if review.author_id != user.id and user.admin is False:
-        raise HTTPException(status_code=403, detail="Forbidden")
-    await review.delete()
-    return {"message": "review deleted"}
+
 
 @router.post("/")
 async def create_review(review: ReviewInPost, user: UserInToken = Depends(get_user_in_token)):
@@ -63,3 +55,14 @@ async def create_review(review: ReviewInPost, user: UserInToken = Depends(get_us
     await reviewInDB.save()
 
     return {"message": "ok"}
+
+# en attente de test
+@router.delete("/{review_id}")
+async def delete_review(review_id: int, user: UserInToken = Depends(get_user_in_token)):
+    review = await Review.get(id=review_id)
+    if review is None :
+        raise HTTPException(status_code=404, detail="Review does not exist")
+    if review.author_id != user.id and user.admin is False:
+        raise HTTPException(status_code=403, detail="Forbidden")
+    await review.delete()
+    return {"message": "review deleted"}
