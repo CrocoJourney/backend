56979e29e5434eed84edcb7dcce466e414298f7c
Maxou <70325779+Maximelego@users.noreply.github.com>
2023-03-30 12:48:55 +0200

    Correction de la route delete Review.

--- app/routers/reviews.py
+++ app/routers/reviews.py
@@ -57,12 +57,14 @@ async def create_review(review: ReviewInPost, user: UserInToken = Depends(get_us
     return {"message": "ok"}
 
 # en attente de test
+
+
 @router.delete("/{review_id}")
 async def delete_review(review_id: int, user: UserInToken = Depends(get_user_in_token)):
-    review = await Review.get(id=review_id)
-    if review is None :
+    review = await Review.get_or_none(id=review_id)
+    if review is None:
         raise HTTPException(status_code=404, detail="Review does not exist")
     if review.author_id != user.id and user.admin is False:
         raise HTTPException(status_code=403, detail="Forbidden")
     await review.delete()
-    return {"message": "review deleted"}
+    return {"message": "ok"}
