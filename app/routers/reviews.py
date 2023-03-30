d9db03199295433bbc64564ca0c7de614f783550
Julien Feraux <56791765+julienferaux@users.noreply.github.com>
2023-03-30 12:13:02 +0200

    Update reviews.py
    ajout delete review

--- app/routers/reviews.py
+++ app/routers/reviews.py
@@ -7,3 +7,14 @@ from app.utils.tokens import get_user_in_token
 
 
 router = APIRouter()
+
+# En attente de test
+@router.delete("/{review_id}")
+async def delete_review(review_id: int, user: UserInToken = Depends(get_user_in_token)):
+    review = await Review.get(id=review_id)
+    if review is None :
+        raise HTTPException(status_code=404, detail="Review does not exist")
+    if review.author_id != user.id and user.admin is False:
+        raise HTTPException(status_code=403, detail="Forbidden")
+    await review.delete()
+    return {"message": "review deleted"}
